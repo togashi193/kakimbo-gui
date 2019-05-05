95833c5b681f3146a91f9d85fae6923a57789b22
togashi193 <iku0425@yahoo.co.jp>
2019-05-05 11:09:31 +0900

    Introduce React and Material-UI

---
+++ src/ApiClient.js
@@ -0,0 +1,33 @@
+class ApiClient {
+  constructor() {
+    this.basePath = `http://localhost:4000`;
+
+    this.defaultOpts = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+  }
+
+  async callApi(path, httpMethod = 'GET', bodyParams = undefined) {
+    const opts = Object.assign({}, this.defaultOpts, {
+      method: httpMethod
+    });
+
+    if (bodyParams) {
+      Object.assign(opts, {
+        body: JSON.stringify(bodyParams)
+      });
+    }
+
+    return await fetch(`${this.basePath}${path}`, opts);
+  }
+
+  async createBilling(params) {
+    return await this.callApi('/billings', 'POST', params);
+  }
+}
+
+ApiClient.instance = new ApiClient();
+export default ApiClient;
